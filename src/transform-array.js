7149f88db1d0b310cf1d7f1d27ecfb434b4be664
Natallia Tkachova <work.tkachova@gmail.com>
2023-05-24 16:51:40 +0300

    feat: add transform-array task solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,11 +13,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error(`'arr' parameter must be an instance of the Array!`);
+  }
+  if (arr.length === 0) {
+    return [];
+  }
+  const sequences = ['--discard-prev', '--discard-next', '--double-prev', '--double-next'];
+  const transformedArr = [...arr];
 
+  arr.forEach((el, ind) => {
+    if (el === '--discard-next' && el !== arr[arr.length - 1]) {
+      transformedArr.splice(ind + 1, 1);
+    }
+    if (el === '--discard-prev' && ind !== 0) {
+      transformedArr.splice(ind - 1, 1);
+    }
+    if (el === '--double-prev' && ind !== 0) {
+      transformedArr.splice(ind - 1, 0, transformedArr[ind - 1]);
+    }
+    if (el === '--double-next' && el !== arr[arr.length - 1]) {
+      transformedArr.splice(ind, 1, transformedArr[ind + 1]);
+    }
+  })
+
+  return transformedArr.filter(el => !sequences.includes(el));
+}
 module.exports = {
   transform
 };
