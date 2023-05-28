0fcec409fbd50fe9961034e7fec54025f4f313ba
Natallia Tkachova <work.tkachova@gmail.com>
2023-05-28 15:20:13 +0300

    feat: add extendet-repeater task solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  if (typeof str !== 'string') {
+    String(str);
+  }
+  if (typeof options.addition !== 'string') {
+    String(options.addition);
+  }
+
+  if (options.repeatTimes > 1 && !options.separator) {
+    options.separator = '+';
+  }
+
+  if (options.addition === undefined) {
+    options.addition = '';
+  }
+
+  if (!options.additionSeparator && options.additionRepeatTimes > 1) {
+    options.additionSeparator = '|';
+  }
+
+  let strWithSeparator = '';
+  const strPart = str + (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes - 1) + options.addition
+
+  for (let i = 0; i < options.repeatTimes - 1; i++) {
+    strWithSeparator += strPart + options.separator;
+  }
+
+  strWithSeparator += strPart;
+
+  return strWithSeparator;
 }
 
 module.exports = {
